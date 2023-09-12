b41e45e0acee20a0b8a4b399309dfab56d93414c
Vernon Leadbeater <vernon.leadbeater@gmail.com>
2023-09-12 00:04:36 -0400

    Solution for 11

--- problems/11-bind-set-time-out.js
+++ problems/11-bind-set-time-out.js
@@ -1,6 +1,10 @@
+const boundFuncTimer = (o, f, d, _=f.bind(o)) => setTimeout(_, d);
+
 function boundFuncTimer(obj, func, delay) {
-  // Your code here
+  const boundFunction = func.bind(obj);
+  setTimeout(boundFunction, delay);
 }
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/

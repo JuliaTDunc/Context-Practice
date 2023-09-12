e990ca635f7475399b36d6e567bcd499a6ba0610
Chanary Wilson <chanarykw@gmail.com>
2023-09-11 20:24:44 -0700

    answered 3 and 4

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,4 +1,18 @@
-// Your code here
+class Dog {
+	constructor(name) {
+		this.name = name;
+	}
+	static makeJet() {
+		let jet = new Dog ('Jet');
+		return jet;
+	}
+	changeName(newName) {
+		this.name = newName;
+	}
+	speak(word) {
+		return `${this.name} says ${word}`
+	}
+}
 
 
 /*****************************************************************************/

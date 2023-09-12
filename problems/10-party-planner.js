82b088e1834410c919f64aeb0b41c8fe34613bf9
Julia Duncan <juliataylorduncan@gmail.com>
2023-09-11 20:55:18 -0700

    09-10

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,5 +1,20 @@
-// Your code here
-
+class PartyPlanner {
+	constructor(guestList) {
+		this.guestList = [];
+	}
+	addToGuestList(name) {
+		this.guestList.push(name);
+	}
+	throwParty() {
+		if(this.guestList.length === 0) {
+			return `Gotta add people to the guest list`
+		}
+		else{
+			let morePeople = this.guestList.join(' and ');
+			return `Welcome to the party ${morePeople}`
+		}
+	}
+}
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
